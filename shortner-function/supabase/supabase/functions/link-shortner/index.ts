117ba263e016739237a34b94d61a6e683f25b879
Saliou Gaye <gayesaliou29@gmail.com>
2022-12-25 11:19:39 +0100

    chore(shortner-function): init supabase locally

--- shortner-function/supabase/supabase/functions/link-shortner/index.ts
+++ shortner-function/supabase/supabase/functions/link-shortner/index.ts
@@ -1,10 +1,6 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
-
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 
-console.log("Hello from Functions!");
+console.log("Link shortner function started");
 
 serve(async (req) => {
 	const { name } = await req.json();
